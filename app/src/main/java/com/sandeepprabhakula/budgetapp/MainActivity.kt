9966df3196bec4f7f93cba8a3df946809f6bee27
sandeep <sandeepprabhakula10@gmail.com>
2022-05-19 23:19:11 +0530

    Added WorkManager to notify user to update expenditure

--- app/src/main/java/com/sandeepprabhakula/budgetapp/MainActivity.kt
+++ app/src/main/java/com/sandeepprabhakula/budgetapp/MainActivity.kt
@@ -4,12 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.work.*
+import com.sandeepprabhakula.budgetapp.workManager.MyWorker
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+        myWorkManager()
+    }
+
+    private fun myWorkManager() {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(false)
+            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val myRequest = PeriodicWorkRequest.Builder(
+            MyWorker::class.java,
+            20,TimeUnit.HOURS
+        ).setConstraints(constraints).build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "my_id",
+                ExistingPeriodicWorkPolicy.KEEP,
+                myRequest
+            )
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/sandeepprabhakula/budgetapp/workManager/MyWorker.kt
@@ -0,0 +1,67 @@
+package com.sandeepprabhakula.budgetapp.workManager
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.sandeepprabhakula.budgetapp.MainActivity
+import com.sandeepprabhakula.budgetapp.R
+
+class MyWorker(context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        showNotification()
+        return Result.success()
+    }
+    companion object {
+        const val CHANNEL_ID = "channel_id"
+        const val NOTIFICATION_ID = 1
+    }
+    private fun showNotification() {
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0, intent, 0
+        )
+
+        val notification = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Add Today's expenditure")
+            .setContentText("Make note of all your expenditures")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channelName = "Channel Name"
+            val channelDescription = "Channel Description"
+            val channelImportance = NotificationManager.IMPORTANCE_HIGH
+
+            val channel = NotificationChannel(CHANNEL_ID, channelName, channelImportance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager = applicationContext.getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+        with(NotificationManagerCompat.from(applicationContext)) {
+            notify(NOTIFICATION_ID, notification.build())
+        }
+    }
+}
